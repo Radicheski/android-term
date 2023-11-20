14253467bfb4a0c06267e73afa9ee7dca11bb2af
Erik Radicheski da Silva <erik.radicheski@gmail.com>
2023-11-20 15:34:47 -0300

    Atualizadas configurações do Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "dev.radicheski.term"
-        minSdk = 16
+        minSdk = 15
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -27,8 +27,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
