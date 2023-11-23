ef7ab6f71adf4e86ae1ea25ecd0fe4f5ac7d5164
Erik Radicheski da Silva <erik.radicheski@gmail.com>
2023-11-23 15:20:05 -0300

    Refatorado Atempt

--- app/src/main/java/dev/radicheski/term/Atempt.java
+++ app/src/main/java/dev/radicheski/term/Atempt.java
@@ -2,58 +2,31 @@ package dev.radicheski.term;
 
 import android.widget.TextView;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import dev.radicheski.term.words.WordRepository;
-
 class Atempt {
 
-    private String word;
     private int cursor = 0;
     private TextView[] letters;
 
-    Atempt(String word, TextView[] views) {
-        this.word = WordRepository.normalize(word);
+    Atempt(TextView[] views) {
         this.letters = views;
     }
 
-    public Answer checkAnswer() {
+    public Answer checkAnswer(String word) {
         if (cursor < letters.length) return Answer.INCOMPLETE_WORD;
 
-        String inputWord = getInputWord();
-        Map<Character, Answer.Case> cases = new HashMap<>();
-
-        if (!WordRepository.checkWord(inputWord)) {
-            clear();
-        } else {
-            for (int i = 0; i < word.length(); i++) {
-                if (!word.contains(inputWord.substring(i, i + 1))) {
-                    cases.put(inputWord.charAt(i), Answer.Case.WRONG_LETTER);
-                    setColor(letters[i], Answer.Case.WRONG_LETTER);
-                } else if (word.charAt(i) == inputWord.charAt(i)) {
-                    cases.put(inputWord.charAt(i), Answer.Case.RIGHT_PLACE);
-                    setColor(letters[i], Answer.Case.RIGHT_PLACE);
-                } else {
-                    cases.put(inputWord.charAt(i), Answer.Case.WRONG_PLACE);
-                    setColor(letters[i], Answer.Case.WRONG_PLACE);
-                }
-            }
-        }
-
-        return new Answer(cases, inputWord);
+        return null; //TODO
     }
 
-    private void setColor(TextView view, Answer.Case _case){
-        view.setTextColor(_case.getTextColor());
-        view.setBackgroundColor(_case.getBackgroundColor());
-    }
-
-    private void clear() {
-        for (TextView view: letters) {
-            view.setText("");
-        }
+    public void clear() {
+        for (TextView view: letters) view.setText("");
         cursor = 0;
+    }
+
+    public void addLetter(String letter) {
+        if (cursor == letters.length) return;
+
+        letters[cursor].setText(letter);
+        cursor++;
     }
 
     public void deleteLetter() {
@@ -63,18 +36,4 @@ class Atempt {
         letters[cursor].setText(null);
     }
 
-    public void addLetter(CharSequence letter) {
-        if (cursor == letters.length) return;
-
-        letters[cursor].setText(letter);
-        cursor++;
-    }
-
-    private String getInputWord() {
-        StringBuilder word = new StringBuilder();
-        for (TextView letter : letters) {
-            word.append(letter.getText());
-        }
-        return WordRepository.normalize(word.toString());
-    }
 }
